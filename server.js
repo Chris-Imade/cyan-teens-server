e84387a9183f5f1f5964660397161437177b2e1f
Chris-Imade <jephthahimade@gmail.com>
2026-03-10 12:35:30 +0000

    updated

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ app.use(cors({
 }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
